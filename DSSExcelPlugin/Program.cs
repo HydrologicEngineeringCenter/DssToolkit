5f4ab160ef8e5452060b51a49e47d2d51d6c2de2
Neema <Neema.G.Nejad@usace.army.mil>
2020-07-30 08:59:26 -0700

    initial commit

---
+++ DSSExcelPlugin/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSSExcelPlugin
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DSSExcelLicensing licensing = new DSSExcelLicensing();
+            licensing.SetPersonalLicense();
+
+
+
+
+        }
+    }
+}
