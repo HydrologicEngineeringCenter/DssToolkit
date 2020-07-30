8eca2c72e5640e36174dc45b6120801c469940b0
Neema <Neema.G.Nejad@usace.army.mil>
2020-07-30 15:45:53 -0700

    added method for converting an excel table to a datatable
    added a test for getting data table from excel file

--- DSSExcelPlugin/DSSExcel.cs
+++ DSSExcelPlugin/DSSExcel.cs
@@ -1,13 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Primitives;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using Hec.Dss;
+using SpreadsheetGear;
+using SpreadsheetGear.Advanced.Cells;
 
 namespace DSSExcelPlugin
 {
     public class DSSExcel
     {
+
+        public SpreadsheetGear.IWorkbookSet workbookSet = SpreadsheetGear.Factory.GetWorkbookSet();
+        public SpreadsheetGear.IWorkbook workbook;
+
+        public DSSExcel(string filename)
+        {
+            workbook = workbookSet.Workbooks.Open(filename);
+        }
+
+        public DataTable ExcelToTimeSeriesTable(string worksheet)
+        {
+            var r = RowCount(worksheet);
+            var c = ColumnCount(worksheet);
+
+            var vals = (IValues)(workbook.Worksheets[worksheet]);
+            DataTable data = new DataTable();
+            for (int i = 0; i < c; i++) { data.Columns.Add(); }
+            var Row = new List<object>();
+           
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    if (vals[i, j].Type == SpreadsheetGear.Advanced.Cells.ValueType.Number)
+                    {
+                        Row.Add(vals[i, j].Number);
+                    }
+                    else if (vals[i, j].Type == SpreadsheetGear.Advanced.Cells.ValueType.Text)
+                    {
+                        Row.Add(vals[i, j].Text);
+                    }
+                }
+                data.Rows.Add(Row.ToArray());
+                Row.Clear();
+            }
+            return data;
+        }
+
+        private int RowCount(string worksheet)
+        {
+            return workbook.Worksheets[worksheet].Cells.CurrentRegion.RowCount;
+        }
+
+        private int ColumnCount(string worksheet)
+        {
+            return workbook.Worksheets[worksheet].Cells.CurrentRegion.ColumnCount;
+        }
+
+
 
 
 
---
+++ DSSExcelTests/BasicFunctionsTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DSSExcelPlugin;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DSSExcelTests
+{
+    [TestClass]
+    public class BasicFunctionsTests
+    {
+        [TestMethod]
+        public void GetTimeSeriesTableFromExcel()
+        {
+            DSSExcel de = new DSSExcel(@"C:\Temp\test.xlsx");
+            var table = de.ExcelToTimeSeriesTable("sheet1");
+            List<object> headers = table.Rows[0].ItemArray.ToList();
+            var t = headers[0].GetType();
+            var h = new List<object>() { "h1", "y1", "x2", "y2" };
+            Assert.AreEqual(t, typeof(string));
+            Assert.IsTrue(headers.SequenceEqual(h));
+            Assert.AreEqual(table.Columns.Count, 4);
+            Assert.AreEqual(table.Rows.Count, 4);
+        }
+    }
+}
