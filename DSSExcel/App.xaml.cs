2c78f221abf4a923969cb75c5183c4731ded6a74
Neema <Neema.G.Nejad@usace.army.mil>
2020-08-27 15:12:29 -0700

    updated all required nuget packages

--- DSSExcel/App.xaml.cs
+++ DSSExcel/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace DSSExcelCLI
+namespace DSSExcel
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- DSSExcel/Properties/Settings.Designer.cs
+++ DSSExcel/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DSSExcelCLI.Properties
+namespace DSSExcel.Properties
 {
 
 
