c8959879194148934a6cdb1b4825b0d149e0f548
Neema <Neema.G.Nejad@usace.army.mil>
2020-09-03 14:54:34 -0700

    added different selection border colors for different record components
    added methods for checking if date/time values are selected properly

--- DSSExcel/DSSExcelVisualSelector.xaml.cs
+++ DSSExcel/DSSExcelVisualSelector.xaml.cs
@@ -1,4 +1,5 @@
-﻿using SpreadsheetGear;
+﻿using Hec.Dss.Excel;
+using SpreadsheetGear;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,10 +21,117 @@ namespace DSSExcel
     /// </summary>
     public partial class DSSExcelVisualEditor : Window
     {
+        public IRange Dates;
+        public IRange Ordinates;
+        public IRange Values;
+
+        public string ComponentSelection
+        {
+            get
+            {
+                return (RecordComponent.SelectedItem as ListBoxItem).Content.ToString();
+            }
+        }
         public DSSExcelVisualEditor(IWorkbook workbook)
         {
             InitializeComponent();
             ExcelWorkbook.ActiveWorkbook = workbook;
+            Dates = ExcelWorkbook.RangeSelection;
+            Ordinates = ExcelWorkbook.RangeSelection;
+            Values = ExcelWorkbook.RangeSelection;
+        }
+
+        private void RecordComponent_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComponentSelection == "Date/Time")
+            {
+                ExcelWorkbook.RangeSelection = Dates == null ? ExcelWorkbook.RangeSelection : Dates;
+                ExcelWorkbook.RangeSelectionBorderBrush = Brushes.Aqua;
+            }
+            else if (ComponentSelection == "Ordinates")
+            {
+                ExcelWorkbook.RangeSelection = Ordinates == null ? ExcelWorkbook.RangeSelection : Ordinates;
+                ExcelWorkbook.RangeSelectionBorderBrush = Brushes.Green;
+            }
+            else if (ComponentSelection == "Values")
+            {
+                ExcelWorkbook.RangeSelection = Values == null ? ExcelWorkbook.RangeSelection : Values;
+                ExcelWorkbook.RangeSelectionBorderBrush = Brushes.Red;
+            }
+        }
+
+        private void ExcelWorkbook_RangeSelectionChanged(object sender, SpreadsheetGear.Windows.Controls.RangeSelectionChangedEventArgs e)
+        {
+            if (ComponentSelection == "Date/Time")
+            {
+                Dates = ExcelWorkbook.RangeSelection;
+            }
+            else if (ComponentSelection == "Ordinates")
+            {
+                Ordinates = ExcelWorkbook.RangeSelection;
+            }
+            else if (ComponentSelection == "Values")
+            {
+                Values = ExcelWorkbook.RangeSelection;
+            }
+        }
+
+        private bool CheckRecordComponents()
+        {
+            if (!CheckDates(Dates))
+                return false;
+
+            return true;
+        }
+
+        private void ImportButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!CheckRecordComponents())
+                return;
+
+            for (int i = 0; i < Dates.RowCount; i++)
+            {
+                
+            }
+            
+            for (int i = 0; i < Ordinates.RowCount; i++)
+            {
+
+            }
+            
+            for (int i = 0; i < Values.RowCount; i++)
+            {
+
+            }
+        }
+
+        private DateTime GetDateTime(double value)
+        {
+            DateTime dt;
+            var b = DateTime.TryParse(ExcelWorkbook.ActiveWorkbook.NumberToDateTime(value).ToString(), out dt);
+            return b ? dt : new DateTime();
+        }
+
+        private bool CheckDates(IRange selection)
+        {
+            if (selection.ColumnCount != 1)
+            {
+                MessageBox.Show("The selection for Date/Time should only have one column of data.", "Date/Time Selection Error",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            for (int i = 0; i < selection.RowCount; i++)
+            {
+                if (selection[i, 0].NumberFormatType != NumberFormatType.DateTime &&
+                    selection[i, 0].NumberFormatType != NumberFormatType.Date)
+                {
+                    MessageBox.Show("All values selected for Date/Time don't follow the date and time format.", "Date/Time Selection Error", 
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
