bb8f478a3eb723d407f1b421ca02c06c2bcacef4
Neema <Neema.G.Nejad@usace.army.mil>
2020-09-22 15:49:59 -0700

    added VM for QuickImport

---
+++ DSSExcel/QuickImportVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSSExcel
+{
+
+    public class QuickImportVM
+    {
+        public bool HasExcelFile { get; set; }
+        public bool HasDssFile { get; set; }
+        public string ExcelFilePath { get; set; }
+        public string DssFilePath { get; set; }
+        public bool OverwriteSheets { get; set; }
+
+        public QuickImportVM()
+        {
+
+        }
+    }
+}
