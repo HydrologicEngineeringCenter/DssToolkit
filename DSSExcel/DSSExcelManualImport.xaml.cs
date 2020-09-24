aae5eca37d963a59ff25d4031ccd103ecb19d90d
Neema <Neema.G.Nejad@usace.army.mil>
2020-09-24 10:18:11 -0700

    removed one task and removed view menu item

--- DSSExcel/DSSExcelManualImport.xaml.cs
+++ DSSExcel/DSSExcelManualImport.xaml.cs
@@ -330,7 +330,6 @@ namespace DSSExcel
         }
     }
 
-    //TODO do more tests with manual imports
     //TODO impliment warning for varying row counts for different columns
     //TODO impliment multi value column selection with manual time series import
     //TODO make manual and quick import work with csv
