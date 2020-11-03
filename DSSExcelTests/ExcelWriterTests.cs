09cb78836df0bfbdc829bb2dc45b1f6247e485be
Neema Nejad <37365315+sharkcooper@users.noreply.github.com>
2020-11-03 15:33:42 -0800

    removed old test source files
    added two new test source files

---
+++ DSSExcelTests/ExcelWriterTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DSSExcelTests
+{
+    [TestClass]
+    public class ExcelWriterTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
