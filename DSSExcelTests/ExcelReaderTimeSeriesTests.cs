c92bc2ebe0604981f0e45360854bfa70faa1d283
Neema Nejad <37365315+sharkcooper@users.noreply.github.com>
2020-11-05 11:03:46 -0800

    added empty ExcelReader tests

--- DSSExcelTests/ExcelReaderTimeSeriesTests.cs
+++ DSSExcelTests/ExcelReaderTimeSeriesTests.cs
@@ -38,5 +38,186 @@ namespace DSSExcelTests
             ExcelReader r = new ExcelReader(filename);
             Assert.AreEqual(r.CheckType(0), Hec.Dss.RecordType.IrregularTimeSeries);
         }
+
+        [TestMethod]
+        public void CheckIndexRegularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void CheckIndexIrregularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetRegularTSTimes()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetIrregularTSTimes()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetRegularTSValues()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetIrregularTSValues()
+        {
+
+        }
+
+        [TestMethod]
+        public void CheckPathRegularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void CheckPathIrregularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void CheckPathLayoutRegularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void CheckPathLayoutIrregularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetRegularTSPath()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetIrregularTSPath()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetRegularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetIrregularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetDataStartRowRegularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetDataStartRowIrregularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetDataStartRowIndexRegularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetDataStartRowIndexIrregularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetPathEndRowRegularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetPathEndRowIrregularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetPathEndRowIndexRegularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetPathEndRowIndexIrregularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetRowCountRegularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetRowCountIrregularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetColumnCountRegularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetColumnCountIrregularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetSmallestColumnCountRegularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetSmallestColumnCountIrregularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void IsRegularTS()
+        {
+
+        }
+
+        [TestMethod]
+        public void IsIrregularTS()
+        {
+
+        }
+
     }
 }
