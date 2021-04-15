64f7a026920f3ab1a1cec4df038f270385553331
Neema Nejad <37365315+sharkcooper@users.noreply.github.com>
2021-04-15 09:45:04 -0700

    added DSSExcelExport as separate project

---
+++ DSSExcelExport/Program.cs
@@ -0,0 +1,62 @@
+﻿using Hec.Dss;
+using Hec.Dss.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSSExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string DssFile = args[1];
+            string ExcelFile = args[2];
+
+            using (DssReader r = new DssReader(DssFile))
+            {
+                object record;
+                ExcelWriter ew = new ExcelWriter(opts.ExcelFile);
+                if (opts.Sheets.ToList().Count == 0)
+                {
+                    for (int i = 0; i < opts.Paths.ToList<string>().Count; i++)
+                    {
+                        string sheet = "import_" + ExcelTools.RandomString(5);
+                        DssPath p = new DssPath(opts.Paths.ElementAt(i));
+                        var type = r.GetRecordType(p);
+                        if (type is RecordType.RegularTimeSeries || type is RecordType.IrregularTimeSeries)
+                        {
+                            record = r.GetTimeSeries(p);
+                            ew.Write(record as TimeSeries, sheet);
+                        }
+                        else if (type is RecordType.PairedData)
+                        {
+                            record = r.GetPairedData(p.FullPath);
+                            ew.Write(record as PairedData, sheet);
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < opts.Sheets.ToList().Count; i++)
+                    {
+                        DssPath p = new DssPath(opts.Paths.ElementAt(i));
+                        var type = r.GetRecordType(p);
+                        if (type is RecordType.RegularTimeSeries || type is RecordType.IrregularTimeSeries)
+                        {
+                            record = r.GetTimeSeries(p);
+                            ew.Write(record as TimeSeries, opts.Sheets.ElementAt(i));
+                        }
+                        else if (type is RecordType.PairedData)
+                        {
+                            record = r.GetPairedData(p.FullPath);
+                            ew.Write(record as PairedData, opts.Sheets.ElementAt(i));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
