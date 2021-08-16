87d92d8ac7848744fcbe1181e18bf2c1632006e3
Neema Nejad <37365315+sharkcooper@users.noreply.github.com>
2021-08-16 13:26:42 -0700

    added config variable to package_release.bat

--- package_release.bat
+++ package_release.bat
@@ -1,2 +1,4 @@
+set config="Release"
+
 Xcopy DSSExcelImport\bin\%config% distribution\dotnet\DSSExcelImport /e /h /c /i /s /y
 Xcopy DSSExcelExport\bin\%config% distribution\dotnet\DSSExcelExport /e /h /c /i /s /y
