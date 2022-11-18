f97bdfc03f94879d155fbaee42634ac3b5582ca2
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2022-11-18 15:54:22 -0800

    add more steps to package_release.bat

--- package_release.bat
+++ package_release.bat
@@ -1,4 +1,16 @@
-set config="Release"
-
+set config="Debug"
+set VERSION=1.0.1-Beta
+set ZIPFILE=dssplugin-DSSExcel.zip
 Xcopy DSSExcelImport\bin\%config% distribution\dotnet\DSSExcelImport /e /h /c /i /s /y
 Xcopy DSSExcelExport\bin\%config% distribution\dotnet\DSSExcelExport /e /h /c /i /s /y
+
+cd distribution
+del %ZIPFILE%
+7z a -r dssplugin-DSSExcel.zip dotnet\*
+
+::  dssexcel 'mil.army.usace.hec:dssplugin-DSSExcel:1.0-Beta@zip'
+
+C:\Programs\apache-maven-3.8.5\bin\mvn deploy:deploy-file -DgroupId=mil.army.usace.hec -DartifactId=dssplugin-DSSExcel -Dversion=%VERSION% -DgeneratePom=true -Dfile=%ZIPFILE% -DrepositoryId=nexus -Dpackaging=zip -Durl=https://www.hec.usace.army.mil/nexus/repository/maven-releases/
+
+
+cd ..
