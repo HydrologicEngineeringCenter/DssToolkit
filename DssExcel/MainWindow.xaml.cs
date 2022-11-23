a52efec5a2bc0b8558757dd74e6ad5a8c99ac26c
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2022-11-23 13:44:28 -0800

    Add new project DssExcel
    Using MVVM

---
+++ DssExcel/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DssExcel
+{
+  public partial class MainWindow : Window
+  {
+    DssExcelViewModel vm;
+    public MainWindow()
+    {
+      InitializeComponent();
+      string[] args = Environment.GetCommandLineArgs();
+      if (args.Length != 3)
+      {
+        MessageBox.Show("No agrguments provided.  Usage:  DssExcel.exe file.xls file.dss");
+        Close();
+      }
+      else
+      {
+        vm = new DssExcelViewModel();
+        vm.ExcelFileName = args[1];
+        vm.DssFileName = args[2];
+
+        mainPanel.Content = new SelectDataTypeView(vm);
+      }
+    }
+  }
+}
---
+++ DssExcel/SelectDataTypeView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DssExcel
+{
+  /// <summary>
+  /// Interaction logic for SelectDataType.xaml
+  /// </summary>
+  public partial class SelectDataTypeView : UserControl
+  {
+    public SelectDataTypeView(DssExcelViewModel vm)
+    {
+      InitializeComponent();
+      DataContext = vm;
+    }
+  }
+}
