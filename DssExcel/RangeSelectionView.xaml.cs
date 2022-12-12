a9b1665315755ba3478e7e07c99392a708398b16
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2022-12-12 12:23:15 -0800

    color selection

--- DssExcel/RangeSelectionView.xaml.cs
+++ DssExcel/RangeSelectionView.xaml.cs
@@ -26,15 +26,13 @@ namespace DssExcel
       this.ExcelView.ActiveWorkbook = vm.ExcelReader.Workbook;
       ExcelView.RangeSelectionChanged += ExcelView_RangeSelectionChanged;
 
-
-
-      
     }
 
     private void ExcelView_RangeSelectionChanged(object sender, SpreadsheetGear.Windows.Controls.RangeSelectionChangedEventArgs e)
     {
       RangeSelectionVM vm = DataContext as RangeSelectionVM;
       vm.RangeSelection = this.ExcelView.RangeSelection;
+      vm.RangeSelection.Interior.Color = SpreadsheetGear.Colors.Khaki;
     }
 
     private void ExcelView_ActiveTabChanged(object sender, SpreadsheetGear.Windows.Controls.ActiveTabChangedEventArgs e)
