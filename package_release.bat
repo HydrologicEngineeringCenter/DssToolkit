728731da434125d7b8d2cb556a9eee4511d97193
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2023-01-09 14:11:19 -0800

    update release batch file

--- package_release.bat
+++ package_release.bat
@@ -1,16 +1,13 @@
 set config="Debug"
-set VERSION=1.0.1-Beta
+set VERSION=1.0.2-Beta
 set ZIPFILE=dssplugin-DSSExcel.zip
-Xcopy DSSExcelImport\bin\%config% distribution\dotnet\DSSExcelImport /e /h /c /i /s /y
-Xcopy DSSExcelExport\bin\%config% distribution\dotnet\DSSExcelExport /e /h /c /i /s /y
+Xcopy DSSExcel\bin\%config% distribution\dotnet\DSSExcel /e /h /c /i /s /y
 
 cd distribution
 del %ZIPFILE%
-7z a -r dssplugin-DSSExcel.zip dotnet\*
+7z a -r  %ZIPFILE% dotnet\*
 
-::  dssexcel 'mil.army.usace.hec:dssplugin-DSSExcel:1.0-Beta@zip'
-
-C:\Programs\apache-maven-3.8.5\bin\mvn deploy:deploy-file -DgroupId=mil.army.usace.hec -DartifactId=dssplugin-DSSExcel -Dversion=%VERSION% -DgeneratePom=true -Dfile=%ZIPFILE% -DrepositoryId=nexus -Dpackaging=zip -Durl=https://www.hec.usace.army.mil/nexus/repository/maven-releases/
+mvn deploy:deploy-file -DgroupId=mil.army.usace.hec -DartifactId=dssplugin-DSSExcel -Dversion=%VERSION% -DgeneratePom=true -Dfile=%ZIPFILE% -DrepositoryId=nexus -Dpackaging=zip -Durl=https://www.hec.usace.army.mil/nexus/repository/maven-releases/
 
 
 cd ..
