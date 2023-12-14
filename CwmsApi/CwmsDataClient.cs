4371c412c20cdf6b32624417d0d8ffa4a1dc1164
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2023-12-14 13:13:10 -0800

    cleanup, re-use Get() method

--- CwmsApi/CwmsDataClient.cs
+++ CwmsApi/CwmsDataClient.cs
@@ -107,22 +107,13 @@ namespace CwmsData.Api
   'https://cwms-data.usace.army.mil/cwms-data/timeseries?name=Mount%20Morris.Elev.Inst.30Minutes.0.GOES-NGVD29-Rev&office=LRB&begin=2023-06-23T06%3A01%3A00&end=2023-06-24T06%3A01%3A00' \
   -H 'accept: application/json;version=2'
        */
-      string endpoint = this.apiUrl = "/timeseries";
       var begin = firstTime.ToString("O");
       var end = lastTime.ToString("O");
 
       string queryString = $"?name={Uri.EscapeDataString(name)}&office={Uri.EscapeDataString(officeID)}&begin={Uri.EscapeDataString(begin)}&end={Uri.EscapeDataString(end)}";
-      string apiUrlWithQuery = endpoint + queryString;
+      string apiUrlWithQuery = this.apiUrl + "/timeseries" + queryString;
 
-      using (HttpClient client = new HttpClient())
-      {
-        client.DefaultRequestHeaders.Accept.Clear();
-        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-        HttpResponseMessage response = await client.GetAsync(apiUrlWithQuery, HttpCompletionOption.ResponseContentRead);
-        response.EnsureSuccessStatusCode();
-
-        string jsonData = await response.Content.ReadAsStringAsync();
+        string jsonData = await Get(apiUrlWithQuery);
 
         var doc = JsonDocument.Parse(jsonData);
         var root = doc.RootElement;
@@ -140,35 +131,35 @@ namespace CwmsData.Api
         ///  "value-count":48,"comment":"value, quality code","values":[[587.03,0],[587.03,0],[587.03,0],[587.03,0],[587.03,0],[587.03,0],[587.02,0],[587.02,0],[587.02,0],[587.02,0],[587.02,0],[587.02,0],[587.01,0],[587.01,0],[587.01,0],[587.01,0],[587.01,0],[587,0],[587,0],[587,0],[587,0],[587,0],[587,0],[586.99,0],[586.99,0],[586.98,0],[586.98,0],[586.98,0],[586.98,0],[586.97,0],[586.97,0],[586.97,0],[586.97,0],[586.97,0],[586.97,0],[586.97,0],[586.97,0],[586.97,0],[586.97,0],[586.97,0],[586.97,0],[586.98,0],[586.98,0],[586.98,0],[586.99,0],[586.99,0],[586.99,0],[587,0]]}
         /// ]}}"
         SimpleTimeSeries rval = new SimpleTimeSeries();
-        if (ts.TryGetProperty("regular-interval-values", out JsonElement rtsv))
+      if (ts.TryGetProperty("regular-interval-values", out JsonElement rtsv))
+      {
+
+        var interval = rtsv.GetProperty("interval").ToString();
+
+        TimeSpan duration = System.Xml.XmlConvert.ToTimeSpan(interval);
+
+        var units = rtsv.GetProperty("unit");
+        var segmentCount = rtsv.GetProperty("segment-count");
+        var segments = rtsv.GetProperty("segments");
+        foreach (JsonElement segment in segments.EnumerateArray())
         {
-
-          var interval = rtsv.GetProperty("interval").ToString();
-
-          TimeSpan duration = System.Xml.XmlConvert.ToTimeSpan(interval);
-
-          var units = rtsv.GetProperty("unit");
-          var segmentCount = rtsv.GetProperty("segment-count");
-          var segments = rtsv.GetProperty("segments");
-          foreach (JsonElement segment in segments.EnumerateArray())
+          var first = segment.GetProperty("first-time").GetDateTime();
+          var last = segment.GetProperty("last-time").GetDateTime();
+          var t = firstTime;
+          var values = segment.GetProperty("values");
+          foreach (JsonElement value in values.EnumerateArray())
           {
-            var first = segment.GetProperty("first-time").GetDateTime();
-            var last = segment.GetProperty("last-time").GetDateTime();
-            var t = firstTime;
-            var values = segment.GetProperty("values");
-            foreach (JsonElement value in values.EnumerateArray())
+            var str = value.EnumerateArray().First().ToString();
+            if (double.TryParse(str, out double v))
             {
-              var str = value.EnumerateArray().First().ToString();
-              if (double.TryParse(str, out double v))
-              {
-                rval.Points.Add((t, v));
-              }
-              t = t.Add(duration);
+              rval.Points.Add((t, v));
             }
-
+            t = t.Add(duration);
           }
 
         }
+
+      }
         return rval;
         /*
          * {
@@ -421,7 +412,7 @@ namespace CwmsData.Api
   }
 }
          */
-      }
+      
 
     }
 
